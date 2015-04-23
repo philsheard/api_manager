e634615a8b3efd35ce12c5252431cbda6e17063f
Phil Sheard <phil@philipsheard.co.uk>
2015-04-23 17:10:21 +1000

    Cleaning up unused functions from utils

--- apimanager/utils.py
+++ apimanager/utils.py
@@ -2,46 +2,8 @@ import numpy as np
 import json
 import logging
 import pandas as pd
-import datetime  # import datetime.fromtimestamp as fromtimestamp
+import datetime
 import pytz
-
-
-def split_series_into_batches(series):
-    array_segment = np.array(series)
-    number_of_segments = array_segment.shape[0]/25
-    batched_arrays = np.array_split(array_segment, number_of_segments)
-    return batched_arrays
-
-
-def extract_data_from_single_batch_response(response, as_type='dict'):
-    # @TODO - MAKE THIS FLEXIBLE TO TAKE DYNAMIC FIELDS
-    import pandas as pd
-    current_response = json.loads(response["body"])
-    results = dict()
-    try:
-        results = {"id": current_response["id"]}
-    except:
-        print current_response
-    if "comments" in current_response:
-        results["comments"] = int(
-                current_response["comments"]["summary"]["total_count"])
-    else:
-        results["comments"] = 0
-    if "likes" in current_response:
-        results["likes"] = int(
-                current_response["likes"]["summary"]["total_count"])
-    else:
-        results["likes"] = 0
-    if "shares" in current_response:
-        results["shares"] = int(
-                current_response["shares"]["count"])
-    else:
-        results["shares"] = 0
-
-    if as_type == "series":
-        return pd.Series(results)
-    else:
-        return results
 
 
 def error_checker(response):
@@ -58,10 +20,10 @@ def error_checker(response):
         # Rate limit error - recommend waiting
         _return_value = "RATELIMIT"
     elif _response_dict.get("error", None):
-        # There was an error in the API from FB. This should be caught by the
-        # status code check for 200, but it's a backstop just in case.
-        raise Exception('Error. Could be innocent, like a rate limit error.')
+        # Unspecified error
+        raise Exception('Error. If repeatable, add to detected errors.')
     else:
+        # Wildcard error capture
         raise Exception('Unknown error. Investigate.')
 
     return _return_value
